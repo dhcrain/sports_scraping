288ca4659d5700ef33bf19e9f0dc3446c7b83515
dhcrain <dhcrain@gmail.com>
2016-07-05 15:15:18 -0400

    functional search on nfl.com can see some stats

--- app/views.py
+++ app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
 
-def sports_scraping_index(responce):
-    pass
+def sports_scraping_index(request):
+    player_name = request.GET.get("player_name") or "John Simon"
+    player_type = request.GET.get("player_type")
+    print(player_type)
+    search_url = "http://www.nfl.com/players/search?category=name&filter={}&playerType={}".format(player_name, player_type)
+    content = requests.get(search_url).text
+    souper = BeautifulSoup(content, "html.parser")
+    elements = souper.find(id="result")
+    base_url = "http://www.nfl.com/"
+    url = base_url + elements.a.attrs['href']
+    content = requests.get(url)
+    souper = BeautifulSoup(content.text, "html.parser")
+    stats = str(souper.find_all(class_="data-table1"))
+    context = {
+    "stats": stats,
+    "player_name": player_name
+    }
+    return render(request, "index.html", context)
